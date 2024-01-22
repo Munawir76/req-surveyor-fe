f152b1067666442813dff0c5cf9cb240153a8736
Munawir76 <munawirsajali76@gmail.com>
2024-01-22 15:52:11 +0700

    change: footer

---
+++ src/constant/wording-footer.jsx
@@ -0,0 +1,43 @@
+const itemsFooter = () => {
+  return [
+    {
+      index: 1,
+      title: 'Company',
+      child: [
+        {
+          name: 'About',
+        },
+        {
+          name: 'Careers',
+        },
+        {
+          name: 'Brand Center',
+        },
+        {
+          name: 'Blog',
+        },
+      ],
+    },
+    {
+      index: 2,
+      title: ' Help center',
+      child: [
+        {
+          name: 'Discord Server',
+        },
+        {
+          name: 'Twitter',
+        },
+        {
+          name: 'Facebook',
+        },
+        {
+          name: 'Contact Us',
+        },
+      ],
+    },
+    
+  ];
+};
+
+export default itemsFooter;
